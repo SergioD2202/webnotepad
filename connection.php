f1f076a4c2a4155579dd5e5178ac5aae53a91d16
SergioD2202 <sergioalejandrods1609@gmail.com>
2021-03-24 01:22:49 +0100

    clever cloud connection

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'notepad');
+define('DB_SERVER', 'bqvkxucejn9m8q2b9l3j-mysql.services.clever-cloud.com');
+define('DB_USERNAME', 'uuxm51iy6cohxzkq');
+define('DB_PASSWORD', 'w8DNyQ7JOqmBSHQ3n0jk');
+define('DB_NAME', 'bqvkxucejn9m8q2b9l3j');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
