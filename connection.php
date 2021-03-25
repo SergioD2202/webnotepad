3fd08db4de70c4dac9eed1d07ccaf2770bb93de2
SergioD2202 <sergioalejandrods1609@gmail.com>
2021-03-25 01:57:38 +0100

    DB config clearDB

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 
 <?php
-//3@-L7rJC$//MAYH?
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'notepad');
+//mysql://b77242d24893b4:0cafa9f8@us-cdbr-east-03.cleardb.com/heroku_818f77c7f856347?reconnect=true
+define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
+define('DB_USERNAME', 'b77242d24893b4');
+define('DB_PASSWORD', '0cafa9f8');
+define('DB_NAME', 'heroku_818f77c7f856347');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
